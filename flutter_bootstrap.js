67d31f83f2b7113d62b7496df9027a272c718cb9
juwel Sheikh <jewelshekh1996@gmail.com>
2025-02-02 22:33:11 +0600

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "996119217"
+    serviceWorkerVersion: "4025291114"
   }
 });
